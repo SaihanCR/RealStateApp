18d2262431ab0b095b64565c486204e69f31afd8
jjjbaezz <87380605+jjjbaezz@users.noreply.github.com>
2025-04-22 15:16:05 -0400

    Agent

--- RealEstateApp/Controllers/AgentController.cs
+++ RealEstateApp/Controllers/AgentController.cs
@@ -1,6 +1,8 @@
 
 
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RealEstateApp.Core.Application.Dtos.Account;
 using RealEstateApp.Core.Application.Helpers;
@@ -8,6 +10,8 @@ using RealEstateApp.Core.Application.Interfaces.Services;
 using RealEstateApp.Core.Application.ViewModels.Property;
 using RealEstateApp.Core.Application.ViewModels.Property.PropertyImage;
 
+using RealEstateApp.Core.Application.ViewModels.User.Client;
+using RealStateApp.Core.Application.ViewModels.Agent;
 
 
 namespace RealEstateApp.Controllers
@@ -43,6 +47,28 @@ namespace RealEstateApp.Controllers
             var propertyList=await _propertyService.MyProperties(agentInSession.Id);
             return View(propertyList);
         }
+
+        //[HttpGet]
+        //public async Task<IActionResult> MyProfile()
+        //{
+        //    var userId = User.FindFirst("UserId")?.Value;
+        //    var model = await _agentService.GetAgentProfileAsync(userId);
+        //    return View(model);
+        //}
+        //[Authorize(Roles = "Agente")]
+
+        //[HttpPost]
+        //public async Task<IActionResult> MyProfile(EditAgentViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return View(model);
+
+        //    var userId = User.FindFirst("UserId")?.Value;
+        //    await _agentService.UpdateAgentProfileAsync(userId, model);
+
+        //    return RedirectToAction("Index", "Home");
+        //}
+        //[Authorize(Roles = "Agente")]
 
         public async Task<IActionResult> PropertyDeatails(int id)
         {
